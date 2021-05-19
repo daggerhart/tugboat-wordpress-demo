d7537c3a924a7bc02f9e20208e79657e1fad533b
James @ Tugboat <james+demo@tugboat.qa>
2021-05-19 11:48:28 -0400

    Install jq; Fix page creation call

--- .tugboat/punchlist-page-create.sh
+++ .tugboat/punchlist-page-create.sh
@@ -3,9 +3,12 @@ set -euo pipefail
 
 export payload=$(printf '{
   "title": "Tugboat Preview: %s (%s)",
-  "url": "%s"
+  "url": "%s",
+  default: false
 }' $TUGBOAT_PREVIEW_NAME $TUGBOAT_PREVIEW_REF $TUGBOAT_DEFAULT_SERVICE_URL)
 
 curl -H "Authorization: Bearer $PUNCHLIST_TOKEN" \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
   -d "$payload" \
   https://app.punchli.st/api/v1/projects/$PUNCHLIST_PROJECT/pages
