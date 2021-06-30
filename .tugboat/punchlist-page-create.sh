7ccc390961c5b8daab0824f48824076073558a02
James @ Tugboat <james+demo@tugboat.qa>
2021-06-30 11:56:41 -0400

    Update json and add newline

--- .tugboat/punchlist-page-create.sh
+++ .tugboat/punchlist-page-create.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export payload=$(printf '{
   "title": "Tugboat Preview: %s (%s)",
   "url": "%s",
-  default: false
+  "default": false
 }' $TUGBOAT_PREVIEW_NAME $TUGBOAT_PREVIEW_REF $TUGBOAT_DEFAULT_SERVICE_URL)
 
 curl -H "Authorization: Bearer $PUNCHLIST_TOKEN" \
