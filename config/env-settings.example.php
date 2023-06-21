148297baa8b7cd86337a6165b3344f9add0ff37d
KeyboardCowboy <chris.albrecht@lullabot.com>
2023-06-21 15:36:14 -0600

    Add local config files.

---
+++ config/env-settings.example.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copy this file to env-settings.php and change the values below accordingly.
+ */
+
+# Set this to the current environment.
+define('WEB_ENV', 'tugboat|local|dev|stage|prod');
+
+# Create a matching file called `wp-config.{WEB_ENV}.php in the `config` directory and replace any
