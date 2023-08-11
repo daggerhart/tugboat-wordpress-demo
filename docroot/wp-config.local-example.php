ee59df6a6afa18a701d443aef8bdb748374aebdc
Jonathan Daggerhart <jonathan@daggerhart.com>
2023-08-11 08:51:39 -0400

    WordPress running in subfolders with locally overridable config

---
+++ docroot/wp-config.local-example.php
@@ -0,0 +1,11 @@
+<?php
+
+// Set these values for the environment this file will serve.
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
+
+// Optionally, set the site's URLs statically or dynamically.
+//define( 'WP_HOME', 'https://localhost');
+//define( 'WP_SITEURL', 'https://localhost/wp' );
---
+++ docroot/wp-content/plugins/my-custom-plugin/my-custom-plugin.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Plugin Name: Example Custom Plugin
+ * Description: Used in Tugboat example.
+ * Version: 1.0.0
+ */
